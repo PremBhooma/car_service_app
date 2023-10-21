f1e71f4fad3519fe5b211dd8da3bdb93a7dc7d74
Prem Bhooma <bhoomasagar1213@gmail.com>
2023-10-21 14:12:11 +0530

    componetns

---
+++ src/Pages/Blog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ComponentsNav from "../Components/ComponentsNav";
+
+const Blog = () => {
+  return (
+    <>
+      <ComponentsNav />
+    </>
+  );
+};
+
+export default Blog;
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -4,7 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../Pages/Login";
 import Signup from "../Pages/Signup";
 import Slot from "../Pages/Slot";
-import Payment from "../Components/Payment";
+// import Payment from "../Components/Payment";
+import Blog from "../Pages/Blog";
+import Faq from "../Pages/Faq";
+import Contact from "../Pages/Contact";
+import Terms from "../Pages/Terms";
+import Privacy from "../Pages/Privacy";
+import Offers from "../Pages/Offers";
+import Reviews from "../Pages/Reviews";
 
 const AllRoutes = () => {
   return (
@@ -14,7 +21,15 @@ const AllRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/slot" element={<Slot />} />
-        <Route path="/payment" element={<Payment />} />
+        {/* <Route path="/payment" element={<Payment />} /> */}
+        {/* <Route path="/spares" element={<Payment />} /> */}
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/reviews" element={<Reviews />} />
       </Routes>
     </div>
   );
